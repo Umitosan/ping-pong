d04e3fb2dfc5b31fc3ca3e25e3ea85df7623313e
Dominic Brown <yeahdom@gmail.com>
2017-03-31 10:42:02 -0700

    add logic to output all numbers from 1 to the user's input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 // BACK-END business logic
 
+var basicCount = function(startNum) {
+  let masterArray = [];
+  let masterStr = "";
 
+  if ((startNum < 1) || (startNum > 200))  {
+    masterStr = "Hey crafty devil... try again using a number between 1 and 200.";
+    return masterStr;
+  }
+
+  for (i=1 ; i <= startNum ; i++) {
+    masterArray.push(i)
+  };
+  
+  masterStr = masterArray.toString();
+  return masterStr;
+};
+
+// var showArrayList = function(displayArray) {
+//   displayArray.forEach(function(arrIndex) {
+//
+//     // $("#out-list li").last().append( "<li>test li</li>" );
+//   });
+// };
 
 
 
@@ -8,8 +30,10 @@
 $(document).ready(function() {
   $("#pongform").submit(function(event) {
   event.preventDefault();
+
   var userInputNum = $("input#userInput").val();
+  $(".output-area").show();
   $(".output-area span#out1").text(userInputNum);
-  $(".output-area span#out2").text("-")
+  $(".output-area #countOut").text(basicCount(userInputNum));
   });
 });
